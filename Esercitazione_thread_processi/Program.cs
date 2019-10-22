4611bb1bd1a7cd1364363bc8a462247f27c5d2b3
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:44:33 +0200

    Controllo il flusso di esecuzione di un thread

--- Esercitazione_thread_processi/Program.cs
+++ Esercitazione_thread_processi/Program.cs
@@ -26,15 +26,37 @@ namespace Esercitazione_thread_processi
             //Console.WriteLine("Main Thread");
             //Console.ReadLine();
 
-            string someVariable = "Matteo Tumiati";
-            var workerThread = new Thread(() =>
-              {
-                  Thread.Sleep(500);
-                  Console.WriteLine("Saluti da: {0}", someVariable);
-              });
+            //string someVariable = "Matteo Tumiati";
+            //var workerThread = new Thread(() =>
+            //  {
+            //      Thread.Sleep(500);
+            //      Console.WriteLine("Saluti da: {0}", someVariable);
+            //  });
 
-            workerThread.Start();
-            someVariable = "Daniele Bochicchio";
+            //workerThread.Start();
+            //someVariable = "Daniele Bochicchio";
+            //Console.ReadLine();
+
+            var list = new List<Thread>();
+            //Qui creiamo ed eseguiamo cinque worker thread
+            for(int index=0; index<5;index++)
+            {
+                var myThread = new Thread((currentIndex) =>
+                  {
+                      Console.WriteLine("Thread {0} è iniziato", currentIndex);
+                      Thread.Sleep(500);
+                      Console.WriteLine("Thread {0} è terminato", currentIndex);
+                  });
+                myThread.Start(index);
+                list.Add(myThread);
+            }
+
+            //Attesa del completamento di ognuno dei worker thread
+            foreach(Thread thread in list)
+            {
+                thread.Join(1000);
+            }
+            Console.WriteLine("esecuzione di tutti i thread terminata");
             Console.ReadLine();
         }
     }
