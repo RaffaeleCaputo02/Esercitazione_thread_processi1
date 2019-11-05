1727372ca7079b84276c01197f35b74e0d0fbb77
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 09:31:53 +0100

    trasformazione di un metodo in un task

---
+++ EsercizioTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EsercizioTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simpleTask = Task.Factory.StartNew(Saluto);
+        }
+
+        private static void Saluto()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Ciao Raffaele");
+        }
+    }
+}
