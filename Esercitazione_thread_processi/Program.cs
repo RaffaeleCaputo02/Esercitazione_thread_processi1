67ad7ef3e2d8f24b4c6b28b1fe5fd8e827841a77
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 08:45:58 +0100

    mettiamo fine ad un thread in esecuzione

--- Esercitazione_thread_processi/Program.cs
+++ Esercitazione_thread_processi/Program.cs
@@ -37,26 +37,44 @@ namespace Esercitazione_thread_processi
             //someVariable = "Daniele Bochicchio";
             //Console.ReadLine();
 
-            var list = new List<Thread>();
-            //Qui creiamo ed eseguiamo cinque worker thread
-            for(int index=0; index<5;index++)
+            //var list = new List<Thread>();
+            ////Qui creiamo ed eseguiamo cinque worker thread
+            //for(int index=0; index<5;index++)
+            //{
+            //    var myThread = new Thread((currentIndex) =>
+            //      {
+            //          Console.WriteLine("Thread {0} è iniziato", currentIndex);
+            //          Thread.Sleep(500);
+            //          Console.WriteLine("Thread {0} è terminato", currentIndex);
+            //      });
+            //    myThread.Start(index);
+            //    list.Add(myThread);
+            //}
+
+            ////Attesa del completamento di ognuno dei worker thread
+            //foreach(Thread thread in list)
+            //{
+            //    thread.Join(1000);
+            //}
+            //Console.WriteLine("esecuzione di tutti i thread terminata");
+            //Console.ReadLine();
+
+            var workerThread = new Thread(() =>
+              {
+                  Console.WriteLine("Inizio di un thread molto lungo");
+                  Thread.Sleep(5000);
+                  Console.WriteLine("Termine worker thread");
+              });
+
+            workerThread.Start();
+            workerThread.Join(500);
+            //Se il worker thread è ancora in esecuzione lo si cancella
+            if(workerThread.ThreadState!=ThreadState.Stopped)
             {
-                var myThread = new Thread((currentIndex) =>
-                  {
-                      Console.WriteLine("Thread {0} è iniziato", currentIndex);
-                      Thread.Sleep(500);
-                      Console.WriteLine("Thread {0} è terminato", currentIndex);
-                  });
-                myThread.Start(index);
-                list.Add(myThread);
+                workerThread.Abort();
             }
 
-            //Attesa del completamento di ognuno dei worker thread
-            foreach(Thread thread in list)
-            {
-                thread.Join(1000);
-            }
-            Console.WriteLine("esecuzione di tutti i thread terminata");
+            Console.WriteLine("Termine applicazione");
             Console.ReadLine();
         }
     }
